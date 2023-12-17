fb4fcc32eab5b219dbe0fda3fc67e80d43c80cd7
talhagngr <111577996+talhagngr@users.noreply.github.com>
2023-12-18 01:19:53 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.getElementById('spotifyLoginButton').addEventListener('click', function() {
     const clientId = '6aaaeb6d3a884d5d94bf46bcdab165e1'; // Replace with your client ID
-    const redirectUri = encodeURIComponent('https://talhagngr.github.io/'); // URL encode the redirect URI
+    const redirectUri = encodeURIComponent('https://talhagngr.github.io/song-match/'); // URL encode the redirect URI
     const scopes = 'user-library-read playlist-read-private playlist-read-collaborative'; // Spotify scopes
     const authUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=code&redirect_uri=${redirectUri}&scope=${encodeURIComponent(scopes)}`;
 
@@ -75,14 +75,13 @@ function displayPlaylists(playlists) {
         playlistContainer.appendChild(checkbox);
         playlistContainer.appendChild(label);
         
-        // Display tracks for each playlist
         if (playlist.tracks) {
             const tracksContainer = document.createElement('div');
             tracksContainer.className = 'tracks-container';
 
             playlist.tracks.forEach(track => {
                 const trackElement = document.createElement('p');
-                trackElement.textContent = track.track.name; // Assuming 'track' has a 'name' property
+                trackElement.textContent = track.track.name;
                 tracksContainer.appendChild(trackElement);
             });
 
@@ -107,7 +106,6 @@ function deselectPlaylist(playlistId) {
 }
 
 function displayError(message) {
-    // Implement UI feedback for errors
     console.error(message); // Placeholder for actual UI implementation
 }
 
@@ -123,7 +121,7 @@ function getAuthorizationCode() {
     }
 }
 
-function exchangeAuthCodeForToken(authCode) {
+async function exchangeAuthCodeForToken(authCode) {
     const clientId = '6aaaeb6d3a884d5d94bf46bcdab165e1'; // Replace with your client ID
     const clientSecret = 'bee9e6ab487d4a3aa70fc310e61fc950'; // Replace with your client secret
 
@@ -134,128 +132,26 @@ function exchangeAuthCodeForToken(authCode) {
 
     const authString = btoa(clientId + ':' + clientSecret); // Base64 encode client ID and secret
 
-    fetch('https://accounts.spotify.com/api/token', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
-            'Authorization': 'Basic ' + authString
-        },
-        body: body
-    })
-    .then(response => {
+    try {
+        const response = await fetch('https://accounts.spotify.com/api/token', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'Authorization': 'Basic ' + authString
+            },
+            body: body
+        });
+
         if (!response.ok) {
             throw new Error('Network response was not ok during token exchange');
         }
-        return response.json();
-    })
-    .then(data => {
+
+        const data = await response.json();
         console.log('Access Token:', data.access_token);
-        return fetchUserPlaylists(data.access_token);
-    })
-    .catch(error => console.error('Error during token exchange:', error));
-}
-
-window.onload = getAuthorizationCode;
-
-// Function to search a track on YouTube
-async function searchYouTube(trackName, artistName, youtubeApiKey) {
-    const query = encodeURIComponent(`${trackName} ${artistName}`);
-    const searchUrl = `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${query}&type=video&key=AIzaSyC6P8DieyB9R6dGogTwtky3vS1o0kAm6eU`;
-
-    try {
-        const response = await fetch(searchUrl);
-        if (!response.ok) {
-            throw new Error(`YouTube Search HTTP error! Status: ${response.status}`);
-        }
-        const data = await response.json();
-        return data.items[0].id.videoId; // Assuming the first result is the desired one
+        fetchUserPlaylists(data.access_token);
     } catch (error) {
-        console.error('Error searching YouTube:', error);
+        console.error('Error during token exchange:', error);
     }
 }
-
-// Function to create a playlist on YouTube
-async function createYouTubePlaylist(title, description, accessToken) {
-    const createPlaylistUrl = `https://www.googleapis.com/youtube/v3/playlists?part=snippet,status`;
-
-    const playlistDetails = {
-        snippet: { title, description },
-        status: { privacyStatus: 'private' }
-    };
-
-    try {
-        const response = await fetch(createPlaylistUrl, {
-            method: 'POST',
-            headers: {
-                'Authorization': 'Bearer ' + accessToken,
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                snippet: { title, description },
-                status: { privacyStatus: 'private' }
-        });
-
-        if (!response.ok) {
-            throw new Error(`YouTube Playlist Creation HTTP error! Status: ${response.status}`);
-        }
-
-        const data = await response.json();
-        console.log('Created YouTube Playlist:', data);
-        return data.id; // Return the created playlist ID
-    } catch (error) {
-        console.error('Error creating YouTube playlist:', error);
-        displayError('Error creating YouTube playlist');
-    }
-}
-
-// Function to add a track to a YouTube playlist
-async function addTrackToYouTubePlaylist(playlistId, trackId, accessToken) {
-    const addToPlaylistUrl = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet`;
-
-    const trackDetails = {
-        snippet: {
-            playlistId,
-            resourceId: {
-                kind: 'youtube#video',
-                videoId: trackId
-            }
-        }
-    };
-
-    try {
-        const response = await fetch(addToPlaylistUrl, {
-            method: 'POST',
-            headers: {
-                'Authorization': 'Bearer ' + accessToken,
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                snippet: {
-                    playlistId: playlistId,
-                    resourceId: {
-                        kind: 'youtube#video',
-                        videoId: trackVideoId
-                    }
-                }
-        });
-
-        if (!response.ok) {
-            throw new Error(`YouTube Add Track HTTP error! Status: ${response.status}`);
-        }
-
-        const data = await response.json();
-        console.log('Added track to YouTube Playlist:', data);
-    } catch (error) {
-        console.error('Error adding track to YouTube playlist:', error);
-        displayError('Error adding track to YouTube playlist');
-    }
-}
-
-// Example usage (you need to implement the logic to call these functions appropriately)
-// searchYouTube('Track Name', 'YouTubeAccessToken').then(trackId => {
-//     createYouTubePlaylist('My Playlist', 'Description', 'YouTubeAccessToken').then(playlistId => {
-//         addTrackToYouTubePlaylist(playlistId, trackId, 'YouTubeAccessToken');
-//     });
-// });
 
 window.onload = getAuthorizationCode;
